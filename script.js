d0092f0e81d4a6c35821d4fc389fd4b2104ecc54
Raul Magno <raulmagno23@gmail.com>
2022-07-17 01:43:58 -0400

    Configurando erros de lint

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
 const submitButton = document.querySelector('.buttonSubmit');
-
-function analysis(event){
-    event.preventDefault();
-    const emailInput = document.getElementById('header-email').value;
-    const passInput = document.getElementById('header-senha').value;
-    
-
-    if (emailInput === 'tryber@teste.com' && passInput === '123456'){
-        alert('Olá, Tryber!');
-    } else {
-        alert('Email ou senha inválidos.');
-    }
+function analysis(event) {
+  event.preventDefault();
+  const emailInput = document.getElementById('header-email').value;
+  const passInput = document.getElementById('header-senha').value;
+  if (emailInput === 'tryber@teste.com' && passInput === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
 }
-submitButton.addEventListener("click", analysis);
+submitButton.addEventListener('click', analysis);
 
 const sendButton = document.getElementById('submit-btn');
 const confirma = document.getElementById('agreement');
-
 function analysis2() {
-    if(confirma.checked) {
-        sendButton.disabled =false;
-    } else {
-        sendButton.disabled = true;
-    }
+  if (confirma.checked) {
+    sendButton.disabled = false;
+  } else {
+    sendButton.disabled = true;
+  }
 }
 confirma.addEventListener('click', analysis2);
